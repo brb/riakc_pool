9c9dec3d615790bc9fd67d6d44264f92a8bde4dd
Martynas Pumputis <martynas.pumputis@gmail.com>
2013-06-05 09:58:36 +0200

    Add tests

---
+++ test/riakcp_tests.erl
@@ -0,0 +1,31 @@
+-module(riakcp_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+-include("riakcp.hrl").
+
+-compile(export_all).
+
+-define(BUCKET, <<"riakcp_tests">>).
+-define(KEY, <<"foobar">>).
+
+%%%=============================================================================
+
+%% Assumes that Riak listener is running on tcp://127.0.0.1:8087.
+riakcp_test_() ->
+    {foreach,
+        fun () -> ok = application:start(?APP) end,
+        fun (_) ->
+            ok = riakcp:exec(delete, [?BUCKET, ?KEY]),
+            ok = application:stop(?APP)
+        end,
+        [
+            {"put/get", fun test_put_and_get/0}
+        ]
+    }.
+
+test_put_and_get() ->
+    ?assertEqual(pong, riakcp:exec(ping, [])),
+
+    Object = riakc_obj:new(?BUCKET, ?KEY, <<"qux">>),
+    ?assertMatch(ok, riakcp:exec(put, [Object])),
+    ?assertMatch({ok, _}, riakcp:exec(get, [?BUCKET, ?KEY])).
